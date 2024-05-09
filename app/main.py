23a60fd9101d62f38be11de959e8c295176c0e7e
Abu Darda <abu.darda@brainstation-23.com>
2024-05-09 17:18:07 +0600

    api: working working

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@
 from fastapi import FastAPI
 from typing import Dict
 from pydantic import BaseModel
-from app.predict import predict_class
-# print(FastAPI.__version__)
+from app.predict import predict_class, get_label
 app = FastAPI()
 
 
@@ -13,43 +12,26 @@ class InputData(BaseModel):
     rotational_speed: float
     torque: float
     tool_wear: float
-    type_H: float
-    type_L: float
-    type_M: float
+    type: str
 
 @app.get("/")
 def home():
     return {"health_check":"ok"}
 
+
 @app.post("/predict")
 def predict(data: InputData):
-    # print(data.data)  # Access the dictionary
     input_dict ={
         "Air temperature [K]": [data.air_temperature],
         "Process temperature [K]": [data.process_temperature],
         "Rotational speed [rpm]":[data.rotational_speed],
         "Torque [Nm]":[data.torque],
         "Tool wear [min]":[data.tool_wear],
-        "Type_H":[data.type_H],
-        "Type_L":[data.type_L],
-        "Type_M":[data.type_M]
+        "Type":[data.type]
+    
     }
-    prediction = predict_class(input_dict)  # Extract the dictionary from InputData
-    return {"prediction": prediction}
-# def predict(data:dict):
-#     print(data)
-#     prediction = predict_class(data)
-#     return {"prediction": prediction}
 
-# dummy = {
-# "Air temperature [K]":[0.5401266308704736],
-# "Process temperature [K]":[0.7152849194585769],
-# "Rotational speed [rpm]":[0.2116504717034246],
-# "Torque [Nm]":[0.4261764736619937], 
-# "Tool wear [min]":[0.8326732168616138],
-# "Type_H":[0.0], 
-# "Type_L":[1.0], 
-# "Type_M":[0.0],
-# }
+    prediction = predict_class(input_dict)
+    predicted_label = get_label(prediction)  # Extract the dictionary from InputData
+    return {"prediction": predicted_label}
 
-# print(predict(dummy))
--- app/predict.py
+++ app/predict.py
@@ -4,49 +4,81 @@ from pathlib import Path
 import pycaret
 from pycaret.classification import *
 import pandas as pd
+import joblib
+import json 
+import numpy as np
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# with open(f"{BASE_DIR}\\model\\predictive_maintenance.pkl","rb") as f:
-#     model = pickle.load(f)
 
 model = load_model(f"{BASE_DIR}\\model\\predictive_maintenance")
 
-classes = {
-    0:"gg"
-}
+
+def get_label(label):
+    with open(f"{BASE_DIR}\\data\\label_mapping.json", 'r') as f:
+        label_mapping = json.load(f)
+    return {v: k for k, v in label_mapping.items()}[label]
+
+features = np.loadtxt(f"{BASE_DIR}\\data\\X_Columns.csv", delimiter =',', dtype=str)
+
+def process(input_df):
+    input_df_encoded = pd.get_dummies(input_df, columns=['Type']).astype('int')
+    missing_cols = set(['Type_H', 'Type_M', 'Type_L']) - set(input_df_encoded.columns)
+    for col in missing_cols:
+        input_df_encoded[col] = 0
+    input_df_encoded = input_df_encoded[features]
+    scaler = joblib.load(f"{BASE_DIR}\\data\\scaler.pkl")
+    input_df_encoded = scaler.transform(input_df_encoded)
+    input_df_encoded = pd.DataFrame(input_df_encoded, columns=features)
+    return input_df_encoded
+    
 
 def predict_class(input):
     input_df=pd.DataFrame(input)
-    predictions = model.predict(input_df)
-    print(predictions)
+    input_df_encoded = process(input_df)
+    print(input_df_encoded)
+    predictions = model.predict(input_df_encoded)
     return predictions.item()
 
 
-# dummy_data = {
-#     'Air temperature [K]': [0.3333333333333286, 0.44444444444444287, 0.22222222222222143, 
-#                             0.7777777777777786, 0.3333333333333286, 0.5185455720329861, 0.5401266308704736],
-#     'Process temperature [K]': [0.5, 0.5, 0.375, 0.625, 0.375, 0.5104547106713903, 0.7152849194585769],
-#     'Rotational speed [rpm]': [0.24039580908032598, 0.3742724097788127, 0.45750873108265433, 
-#                                0.11745928319466402, 0.11397964130389841, 0.20133893627883215, 0.2116504717034246],
-#     'Torque [Nm]': [0.3698630136986301, 0.2876712328767123, 0.2465753424657534, 0.6301369863013699, 
-#                     0.726027397260274, 0.5205679573942655, 0.4261764736619937],
-#     'Tool wear [min]': [0.2569169960474308, 0.3043478260869565, 0.1660079051383399, 0.14712147436778086, 
-#                         0.8540889831344971, 0.40246624225521516, 0.8326732168616138],
-#     'Type_H': [0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-#     'Type_L': [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0],
-#     'Type_M': [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dick = {
+#     "Air temperature [K]": [298.9],
+#     "Process temperature [K]": [309.2],
+#     "Rotational speed [rpm]": [1412],
+#     "Torque [Nm]": [44.1],
+#     "Tool wear [min]": [140],
+#     "Type": ["M"]
 # }
 
-# dummy_df = pd.DataFrame(dummy_data)
-# dummy = {
-# "Air temperature [K]":[0.5401266308704736],
-# "Process temperature [K]":[0.7152849194585769],
-# "Rotational speed [rpm]":[0.2116504717034246],
-# "Torque [Nm]":[0.4261764736619937], 
-# "Tool wear [min]":[0.8326732168616138],
-# "Type_H":[0.0], 
-# "Type_L":[1.0], 
-# "Type_M":[0.0],
-# }
-# print(predict_class(dummy))
-# print(type(model))
+# print(predict_class(dick))
+
+
+# ['Air temperature [K]',
+#   'Process temperature [K]',
+#   'Rotational speed [rpm]',
+#   'Torque [Nm]', 'Tool wear [min]',
+#   'Type_H', 'Type_L', 'Type_M']
+    
